fb77ad201a2bb9ba85cc1b22e837079635a151d7
André Michelle <andre.michelle@gmail.com>
2025-07-07 11:36:51 +0200

    starting

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,5 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/index.js"],
+  extends: ["@opendaw/eslint-config/index.js"],
 };
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,8 +1,12 @@
-import { Header } from "@repo/ui/header";
-import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Counter } from "@repo/ui/counter";
-import { setupCounter } from "@repo/ui/setup-counter";
+import {Header} from "@opendaw/ui/header"
+import "./style.css"
+import typescriptLogo from "/typescript.svg"
+import {Counter} from "@opendaw/ui/counter"
+import {setupCounter} from "@opendaw/ui/setup-counter"
+import {tryProvide} from "@opendaw/lib-std"
+import {PPQN} from "@opendaw/lib-dsp"
+
+console.debug(tryProvide(() => 5), PPQN.toString(PPQN.Bar))
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -12,11 +16,11 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    ${Header({ title: "Web" })}
+    ${Header({title: "Web"})}
     <div class="card">
       ${Counter()}
     </div>
   </div>
-`;
+`
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)
