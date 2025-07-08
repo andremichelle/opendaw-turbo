2c1023d3136021ebd121815417dffdc3f4bc9f06
André Michelle <andre.michelle@gmail.com>
2025-07-08 18:54:02 +0200

    lib detection in jsx

--- packages/libs/jsx/src/index.ts
+++ packages/libs/jsx/src/index.ts
@@ -1,3 +1,12 @@
+const key = Symbol.for("@openDAW/lib-jsx")
+
+if ((globalThis as any)[key]) {
+    console.debug(`%c${key.description}%c is already available in ${globalThis.constructor.name}.`, "color: hsl(10, 83%, 60%)", "color: inherit")
+} else {
+    (globalThis as any)[key] = true
+    console.debug(`%c${key.description}%c is now available in ${globalThis.constructor.name}.`, "color: hsl(200, 83%, 60%)", "color: inherit")
+}
+
 export * from "./types"
 export * from "./create-element"
 export * from "./inject"
@@ -9,5 +18,3 @@ export * from "./std/Hotspot"
 export * from "./std/LocalLink"
 export * from "./std/Router"
 export * from "./std/Preloader"
-
-console.debug("lib-jsx imported")
